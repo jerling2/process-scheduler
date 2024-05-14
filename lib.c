6fa7d4d5c9d4e44b41f2b432370108b4c583499e
Joseph Erlinger <josepherlinger@gmail.com>
2024-05-14 12:12:26 -0700

    Add documentation to 'numtok'

--- lib.c
+++ lib.c
@@ -105,33 +105,41 @@ cmd *parseline (char *line, const char *delim)
 }   /* parseline */
 
 
+/**
+ * @brief Count the number of tokens in a buffer.
+ * 
+ * This function uses a finite state machine pattern to count the number of
+ * tokens in a buffer based on a input delimiter.
+ * 
+ * @caution This function has a chance to modify the input buffer.
+ * 
+ * @param[in,out] buffer Pointer that will be read from. (Note Caution)
+ * @param[in] delimeter to distinguish the start/end of a token.
+ * @return integer count of the number of tokens in the buffer.
+ */
 int numtok (char *buf, const char *delim)
 {
     int tok;      // Number of tokens.
     int state;    // The current state of the algorithm.
-    int i;        // The ith char in the buf.
+    int i;        // The ith character in the buf.
 
-    tok = 1;                   //< Start at 1 to make space for the null token.
+    tok = 1;                    // Start at 1 to make space for the null token.
     state = 0;
     i = 0;
-    
-    /* Strip newline character (if any) */
-    strtok (buf, "\n"); 
-    
+    strtok (buf, "\n");                    // Strip newline character (if any).
     /* Skip all leading delimiters */
     while (buf[i] == delim[0]) {
         i ++;
     }	
-    
     /* Increment tokens only when moving from state 0 to 1 */
-    for (; buf[i] != '\0'; i++) {
+    for (; buf[i] != '\0'; i++) {  
         /* State 0: Skip all delimiters */
-        if (state == 0 && buf[i] != delim[0]) {
+        if (state == 0 && buf[i] != delim[0]) { // 
             state = 1;
-            tok ++;                                       //< Increment tokens.
+            tok ++;                                       // Increment tokens.
         }
         /* State 1: Skip all non-delimiters */
-        if (state == 1 && buf[i] == delim[0]) {
+        if (state == 1 && buf[i] == delim[0]) { //  
             state = 0;
         }
     }
