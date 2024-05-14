e4c276dfee7f5c953348caff73d2bfcaaeed39a2
Joseph Erlinger <josepherlinger@gmail.com>
2024-05-13 23:00:12 -0700

    Improve enqueue and dequeue in 'queue.c'

--- queue.c
+++ queue.c
@@ -1,1 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "queue.h"
+
+/*
+typedef struct queue {
+    void *head;
+    void *tail;
+    int size;
+} queue;
+*/
+
+void enqueue(queue *q, void *data)
+{
+    node *tail;
+    node *new;
+
+    /* Initialize new node */
+    new = (node*)malloc(sizeof(node));
+    new->prev = NULL;
+    new->next = NULL;
+    new->data = data;
+
+    /* Edge case: the first enqueue */
+    if (q->size == 0) {
+        q->head = new;
+        q->tail = new;
+        q->size ++;
+        return;
+    }
+    
+    /* Otherwise, default enqueue */
+    tail = q->tail;
+    tail->next = new;
+    new->prev = tail;
+    q->tail = new;
+    q->size ++;
+}
+
+
+void *dequeue(queue *q)
+{
+    node *head;
+    node *new;
+    void *data;
+    
+    /* Grab the head*/
+    head = q->head;
+    data = head->data;
+
+    /* Edge case: empty queue */
+    if (q->size == 0) {
+        return NULL;
+    }
+
+    /* Edge case: size 1 queue */
+    if (q->size == 1) {
+        q->head = NULL;
+        q->tail = NULL;
+        q->size --;
+        free(head);
+        return data;
+    }
+
+    /* Otherwise, default dequeue */
+    new = head->next;
+    new->prev = NULL;
+    q->head = new;
+    q->size --;
+    free(head);
+    return data;
+}
+
+void freequeue(queue *q, freefun fun)
+{
+    node *n;
+    while ((n = dequeue(q)) != NULL) {
+        fun(n->data);
+    }
+    free(q);
+}
